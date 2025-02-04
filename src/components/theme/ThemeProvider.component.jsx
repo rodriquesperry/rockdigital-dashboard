044942d58a9d6b04ea5f5d740acf6a46569acdc3
Rodriques <rodriques.perry@gmail.com>
2025-02-03 18:54:06 -0600

    Fix dark mode in Mui Cards

--- src/components/theme/ThemeProvider.component.jsx
+++ src/components/theme/ThemeProvider.component.jsx
@@ -1,26 +1,42 @@
-"use client";
+'use client';
 
-import { createContext, useContext, useState } from "react";
-import { GeistProvider, CssBaseline } from "@geist-ui/core";
+import { createContext, useContext, useState, useMemo } from 'react';
+import { GeistProvider, CssBaseline } from '@geist-ui/core';
+import {
+	ThemeProvider as MuiThemeProvider,
+	createTheme,
+} from '@mui/material/styles';
 
 // Create Theme Context
 const ThemeContext = createContext();
 
 // Theme Provider Component
 export const ThemeProvider = ({ children }) => {
-	const [themeType, setThemeType] = useState("light");
+	const [themeType, setThemeType] = useState('light');
 
 	// Function to toggle theme
 	const switchThemes = () => {
-		setThemeType((prev) => (prev === "dark" ? "light" : "dark"));
+		setThemeType((prev) => (prev === 'dark' ? 'light' : 'dark'));
 	};
+
+	const muiTheme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: themeType,
+				},
+			}),
+		[themeType]
+	);
 
 	return (
 		<ThemeContext.Provider value={{ themeType, switchThemes }}>
-			<GeistProvider themeType={themeType}>
-				<CssBaseline />
-				{children}
-			</GeistProvider>
+			<MuiThemeProvider theme={muiTheme}>
+				<GeistProvider themeType={themeType}>
+					<CssBaseline />
+					{children}
+				</GeistProvider>
+			</MuiThemeProvider>
 		</ThemeContext.Provider>
 	);
 };
