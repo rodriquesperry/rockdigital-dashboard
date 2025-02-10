2baaec9aee7fcbc23aff4aa77c118336ddb43357
Rodriques <rodriques.perry@gmail.com>
2025-02-10 17:21:12 -0600

    Update: remove all dashboard components and dependency to mui/x-charts

--- src/components/dashboard/Dashboard.component.jsx
+++ src/components/dashboard/Dashboard.component.jsx
@@ -8,28 +8,7 @@ import styles from './dashboard.module.css';
 import { green } from '@mui/material/colors';
 
 const Dashboard = () => {
-	return (
-		<div>
-			<div className={styles.card}>
-				<div className={styles.top_kpi}>
-					<TotalCountKPI title={'Total Leads'} count={1500} />
-					<TotalCountKPI title={'Total Customers'} count={800} />
-					<TotalCountKPI title={'Monthly Earnings'} count={'$6,820'} />
-				</div>
-				<div className={styles.dashboard_main}>
-					<div className={styles.second_row}>
-						<CustomerRelationsChart />
-						<div className={styles.second_row_small_container}>
-              <ConversionRate  />
-						</div>
-					</div>
-					<EmailsOverview />
-					<EmailsOverview />
-				</div>
-				<div className={styles.top_kpi}></div>
-			</div>
-		</div>
-	);
+	return <div></div>;
 };
 
 export default Dashboard;
