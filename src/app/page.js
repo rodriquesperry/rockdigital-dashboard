171fee093e5b968d35c0f6efe9a16f1dbdbc9b44
Rodriques <rodriques.perry@gmail.com>
2025-02-10 18:38:41 -0600

    Add: shadcn and tailwindcss to project

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
-import CustomerRelationsChart from '@/components/chart_customer_relations/CustomerRelationsChart';
-import YearlyBreakupKPI from '@/components/kpi_total_count/TotalCount';
 import Dashboard from '@/components/dashboard/Dashboard.component';
-import Card from '@mui/material/Card';
-import Image from 'next/image';
 
 import styles from './page.module.css';
 
--- src/components/dashboard/Dashboard.component.jsx
+++ src/components/dashboard/Dashboard.component.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import CustomerRelationsChart from '@/components/chart_customer_relations/CustomerRelationsChart';
-import EmailsOverview from '../welcome_emails_overview/EmailsOverviewChart';
-import TotalCountKPI from '@/components/kpi_total_count/TotalCount';
-import ConversionRate from '../conversion_rate/ConversionRate.component';
-
 import styles from './dashboard.module.css';
 import { green } from '@mui/material/colors';
 
