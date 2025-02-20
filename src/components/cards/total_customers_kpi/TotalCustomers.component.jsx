fd43f8f887174d77e4a9d4dc1fe91a205bb27fc4
Rodriques <rodriques.perry@gmail.com>
2025-02-19 20:39:29 -0600

    Update: total leads and total customers add arrows to show trending up or down and percentage from last month

--- src/components/cards/total_customers_kpi/TotalCustomers.component.jsx
+++ src/components/cards/total_customers_kpi/TotalCustomers.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Card } from '@/components/ui/card';
+import { LuCircleArrowUp } from 'react-icons/lu';
+import { LuCircleArrowDown } from "react-icons/lu";
 
 import styles from './total_customers.module.css';
 
 const TotalCustomers = () => {
+  const percentage = 0;
+
 	return (
 		<Card className={`${styles.container} flex flex-col card`}>
 			<p className={`${styles.title} card_title`}>Total Customers</p>
 			<div className={styles.total_customers_container}>
 				<p className={styles.total_customers_num}>898</p>
+				<span>
+					{percentage > 0 ? <LuCircleArrowUp size={30} className={styles.green} /> : <LuCircleArrowDown size={30} className={styles.red}/>
+        } &nbsp;4.7% from mth
+				</span>
 			</div>
 		</Card>
 	);
--- src/components/cards/total_leads_kpi/TotalLeads.component.jsx
+++ src/components/cards/total_leads_kpi/TotalLeads.component.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Card } from '@/components/ui/card';
+import { LuCircleArrowUp } from 'react-icons/lu';
+import { LuCircleArrowDown } from "react-icons/lu";
 
 import styles from './total_leads.module.css';
 
 const TotalLeads = () => {
+  const percentage = 1.7;
+
 	return (
 		<Card className={`${styles.container} flex flex-col card`}>
 			<p className={`${styles.title} card_title`}>Total Leads</p>
-      <div className={styles.total_leads_container}>
-        <p className={styles.total_leads_num}>1,298</p>
-      </div>
+			<div className={styles.total_leads_container}>
+				<p className={styles.total_leads_num}>1,298</p>
+				<span>
+					{percentage > 0 ? <LuCircleArrowUp size={30} className={styles.green} /> : <LuCircleArrowDown size={30} className={styles.red}/>
+        } &nbsp;4.7% from mth
+				</span>
+			</div>
 		</Card>
 	);
 };
 
 export default TotalLeads;
+
+// ***line 13*** percentage > 0.1 ? arrow green up : arrow red down
