d7a8cc694fc6d249f8840c23a7e87306d89da586
Rodriques <rodriques.perry@gmail.com>
2025-02-19 20:11:27 -0600

    Update: Toatl Leads and Total Cutaomers Cards complete

--- src/components/cards/total_customers_kpi/TotalCustomers.component.jsx
+++ src/components/cards/total_customers_kpi/TotalCustomers.component.jsx
@@ -4,7 +4,14 @@ import { Card } from '@/components/ui/card';
 import styles from './total_customers.module.css';
 
 const TotalCustomers = () => {
-	return <Card className={`${styles.container} flex flex-col card`}></Card>;
+	return (
+		<Card className={`${styles.container} flex flex-col card`}>
+			<p className={`${styles.title} card_title`}>Total Customers</p>
+			<div className={styles.total_customers_container}>
+				<p className={styles.total_customers_num}>898</p>
+			</div>
+		</Card>
+	);
 };
 
 export default TotalCustomers;
--- src/components/cards/total_leads_kpi/TotalLeads.component.jsx
+++ src/components/cards/total_leads_kpi/TotalLeads.component.jsx
@@ -4,7 +4,14 @@ import { Card } from '@/components/ui/card';
 import styles from './total_leads.module.css';
 
 const TotalLeads = () => {
-	return <Card className={`${styles.container} flex flex-col card`}></Card>;
+	return (
+		<Card className={`${styles.container} flex flex-col card`}>
+			<p className={`${styles.title} card_title`}>Total Leads</p>
+      <div className={styles.total_leads_container}>
+        <p className={styles.total_leads_num}>1,298</p>
+      </div>
+		</Card>
+	);
 };
 
 export default TotalLeads;
